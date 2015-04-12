661330cf5c6e19225eb687bcda03ef02bb479e8b
Gianluca Pane <gianluca_pane@brown.edu>
2015-04-12 14:41:44 -0400

    Added an implementation of insertion sort

--- Sorting/src/Sorting/InsertionSort.java
+++ Sorting/src/Sorting/InsertionSort.java
@@ -1,6 +1,21 @@
 package Sorting;
 
 public class InsertionSort {
+
+	public static void printArray(int[] arr){
+		for (int i = 0; i < arr.length; i++){
+			System.out.printf("%d ", arr[i]);
+		}
+		System.out.println();
+	}
+
+	public static int[] swap (int[] array, int i, int j){
+		int temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+		return array;
+	}
+
 	/**
 	 * insertionSort			sorts an array of integers using insertion sort
 	 * 
@@ -10,6 +25,20 @@ public class InsertionSort {
 	public static int[] insertionSort(int[] unsortedArray) {
 		//TODO: S2 implements insertion sort
 		
-		return null;
+		for (int i = 1; i < unsortedArray.length; i++){
+			int j = i;
+			while (j > 0 && unsortedArray[j-1] > unsortedArray[j]){
+				unsortedArray = swap(unsortedArray, j, j-1);
+				j--;
+			}
+		}
+		return unsortedArray;
 	}
+
+	// public static void main(String[] args){
+	// 	int[] toSort = new int[] { 5,24, 25,2,1,5,8,3,45,2};
+	// 	int[] sorted = insertionSort(toSort);
+
+	// 	printArray(toSort);
+	// }
 }
--- Sorting/src/Sorting/Main.java
+++ Sorting/src/Sorting/Main.java
@@ -24,10 +24,9 @@ public class Main {
 		
 		System.out.println("Sorted Array:");
 		//TODO: sort the array and print out its contents
-		BubbleSort.bubbleSort(unsortedArray);
-		for (int i: unsortedArray) {
-				System.out.print(i + " ");
-		}
+		
+		InsertionSort.printArray(InsertionSort.insertionSort(unsortedArray));
+		
 	}
 
 }
